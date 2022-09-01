be9110232599e5515650832a903a08e6028ef0b2
ZAMLER <jekaninho7@gmail.com>
2022-09-01 20:02:01 +0300

    Перший Урок

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("write first operand");
+var input = Console.ReadLine();
+int operand1 = int.Parse(input);
+Console.WriteLine("write second operand");
+input = Console.ReadLine();
+int operand2 = int.Parse(input);
+Console.WriteLine("write operation");
+input = Console.ReadLine();
+switch (input)
+{
+    case "+":
+        Console.WriteLine($"operand1+operand2={operand1 + operand2}");
+        break;
+    case "-":
+        Console.WriteLine($"operand1-operand2={operand1 - operand2}");
+        break;
+    case "*":
+        Console.WriteLine($"operand1*operand2={operand1 * operand2}");
+        break;
+    case "/":
+        Console.WriteLine($"operand1/operand2={operand1 / operand2}");
+        break;
