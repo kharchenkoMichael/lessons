ca41928acb752bec6347ce8047dfc03bde308fde
ZAMLER <jekaninho7@gmail.com>
2022-09-05 19:38:31 +0300

    Классна робота урок 2

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,19 +1,74 @@
-﻿int a = 5;
-int b = 8;
-int c = 7;
-Console.WriteLine ($"a+b={a+b}");
-Console.WriteLine($"a-b={a - b}");
-Console.WriteLine($"a*b={a * b}");
-Console.WriteLine($"a/b={a / b}");
-Console.WriteLine($"a%b={a % b}");
+﻿
+bool result = true;
+int value = 0;
 
-Console.WriteLine($"(a+b+c) / 3={(a + b + c) / 3.0}");
+do
+{
+   Console.WriteLine("write number");
+   string inputString = Console.ReadLine();
+   result = int.TryParse(inputString, out int intValue);
+   value = intValue;
+} while (!result);
+
+for (int i = 0; i <= value; i+=2)
+{
+    
+    
+        Console.WriteLine(i);
+    
+  
+}
+bool isChating = true;
+while (isChating)
+{
+    Console.WriteLine("write something");
+    string input = Console.ReadLine();
+    switch (input)
+    {
+        case "hello":
+            Console.WriteLine("Hello");
+            break;
+        case "how are you":
+            Console.WriteLine("i'm fine");
+            break;
+        case "what is your name":
+            Console.WriteLine("i'm console application");
+            break;
+        case "exit":
+            isChating = false;
+            break;
+        default:
+            Console.WriteLine("i don't understand you");
+            break;
+
+    }
+}
+int a = 0;
+while (true)
+{
+    string input = Console.ReadLine();
+    switch (input)
+    {
+        case "+":
+            a++;
+            break;
+        case "-":
+            a--;
+            break;
+        default:
+            Console.WriteLine(a);
+            break;
+
+    }
+}
+
+   
 
 
-double pi = 3.14;
-Console.WriteLine("input r");
-int r = int.Parse(Console.ReadLine());
-Console.WriteLine($"S={pi*r*r}") ;
+
+
+
+
 
 
 
