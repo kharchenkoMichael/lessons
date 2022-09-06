180477596064044e76948029a55218beacda64ac
ZAMLER <jekaninho7@gmail.com>
2022-09-06 16:21:35 +0300

    дз задача 1

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,66 +1,23 @@
-﻿
-bool result = true;
+﻿bool result = true;
 int value = 0;
 
 do
 {
-   Console.WriteLine("write number");
-   string inputString = Console.ReadLine();
-   result = int.TryParse(inputString, out int intValue);
-   value = intValue;
+    Console.WriteLine("write number");
+    string inputString = Console.ReadLine();
+    result = int.TryParse(inputString, out int intValue);
+    value = intValue;
 } while (!result);
+int i = 0;
+for (i = 0; i <= 0; i++);
 
-for (int i = 0; i <= value; i+=2)
-{
-    
-    
-        Console.WriteLine(i);
-    
-  
-}
-bool isChating = true;
-while (isChating)
-{
-    Console.WriteLine("write something");
-    string input = Console.ReadLine();
-    switch (input)
-    {
-        case "hello":
-            Console.WriteLine("Hello");
-            break;
-        case "how are you":
-            Console.WriteLine("i'm fine");
-            break;
-        case "what is your name":
-            Console.WriteLine("i'm console application");
-            break;
-        case "exit":
-            isChating = false;
-            break;
-        default:
-            Console.WriteLine("i don't understand you");
-            break;
 
-    }
-}
-int a = 0;
-while (true)
 {
-    string input = Console.ReadLine();
-    switch (input)
-    {
-        case "+":
-            a++;
-            break;
-        case "-":
-            a--;
-            break;
-        default:
-            Console.WriteLine(a);
-            break;
-
-    }
+    if (i % 3 == 0) ;
+    Console.WriteLine(i);
 }
+
+
 
    
 
