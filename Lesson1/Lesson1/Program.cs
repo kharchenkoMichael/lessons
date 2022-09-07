7d2707c7cce4d67535d03fe74e65d8fc947e82bb
ZAMLER <jekaninho7@gmail.com>
2022-09-07 19:01:38 +0300

    Method 3

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,39 +1,20 @@
 ﻿
-
-void Add(int a,int b)
-{
-    Console.WriteLine(a+b);
-}
-void Sub(int a,int b)
-{
-    Console.WriteLine(a-b);
-}
-void Mult(int a,int b)
-{
-    Console.WriteLine(a*b);
-}
-void Div(int a, int b)
-{
-    
-    if (b == 0)
-    {
-        Console.WriteLine("на нуль дилыты не можна");
-        return;
-    }
-    Console.WriteLine(a / b);
-}
-string input = Console.ReadLine();
-string input1 = Console.ReadLine();
-int.TryParse(input,out int a);
-int.TryParse(input1, out int b);
-Add(a, b);
-Sub(a, b);
-Mult(a, b);
-Div(a, b);
+//double ChangeUaInDollar(double ua)
+//{
+//    return ua / 41;
+//}
+//double.TryParse(Console.ReadLine(), out double ua);
+//Console.WriteLine(ChangeUaInDollar(ua));
 
 
-
-
+//void Method(int a = 5, string b = "hello")
+//{
+//    Console.WriteLine(a + b);
+//}
+//Method(10, "bye");
+//Method(20);
+//Method(b: "bye");
+//Method();
 
 
 
