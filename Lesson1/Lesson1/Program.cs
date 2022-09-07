5eb1761c2b9c4b5c41f049b70e93f6cf77d29954
ZAMLER <jekaninho7@gmail.com>
2022-09-07 15:47:47 +0300

    задача 2

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,10 +1,54 @@
-﻿double pi = 3.14;
-Console.WriteLine("input r");
-Console.WriteLine("input h");
-int r = int.Parse(Console.ReadLine());
-int h = int.Parse(Console.ReadLine());
-Console.WriteLine($"V={pi * r * r * h}");
-Console.WriteLine($"S={2 * pi * r*(r + h)}");
+﻿//double pi = 3.14;
+//Console.WriteLine("input r");
+//Console.WriteLine("input h");
+//int r = int.Parse(Console.ReadLine());
+//int h = int.Parse(Console.ReadLine());
+//Console.WriteLine($"V={pi * r * r * h}");
+//Console.WriteLine($"S={2 * pi * r*(r + h)}");
+
+
+
+bool isChating = true;
+while (isChating)
+{
+    string input = (Console.ReadLine());
+    switch (input)
+    {
+        case "дождь":
+            Console.WriteLine("Rain");
+            break;
+        case "солнце":
+            Console.WriteLine("Sun");
+            break;
+        case "снег":
+            Console.WriteLine("Snow");
+            break;
+        case "ураган":
+            Console.WriteLine("Hurricane");
+            break;
+        case "наводнение":
+            Console.WriteLine("Flood");
+            break;
+        case "ветер":
+            Console.WriteLine("Wind");
+            break;
+        case "холодно":
+            Console.WriteLine("Cold");
+            break;
+        case "тепло":
+            Console.WriteLine("Warm");
+            break;
+        case "тучи":
+            Console.WriteLine("cloud");
+            break;
+        case "землетрясение":
+            Console.WriteLine("earthquake");
+            break;
+        default:
+            Console.WriteLine("i'm not understand you");
+            break;
+    }
+}
 
 
 
