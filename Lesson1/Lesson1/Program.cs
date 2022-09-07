095f81465a8a0051b749024d3910bf16c6f646f4
ZAMLER <jekaninho7@gmail.com>
2022-09-07 19:36:43 +0300

    Rekyrsia

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -11,10 +11,15 @@
 //Console.WriteLine(Sum(10));
 
 
-
-
-
-
+//int Factorial(int a)
+//{
+//    if (a==0)
+//    {
+//        return 1;
+//    }
+//    return Factorial(a - 1) * a;
+//}
+//Console.WriteLine(Factorial(5));
 
 
 
