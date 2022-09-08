39b2db123eea63e8c006f32e9c812f5fb7d069a3
ZAMLER <jekaninho7@gmail.com>
2022-09-08 18:37:13 +0300

    масивы зубчастый

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,12 +1,13 @@
-﻿int[,] array = new int[5, 4];
+﻿int[][] array = new int[3][];
+array[0] = new int[] { 1, 2, 3 };
+array[1] = new int[] { 1, 2 };
+array[2] = new int[] { 1, 2, 3, 4 };
 
-array[2, 3] = 1;
-
-for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+    for (int j = 0; j < array[i].Length; j++)
     {
-        Console.Write(array[i, j]);
+        Console.Write(array[i][j]);
     }
     Console.WriteLine();
 }
