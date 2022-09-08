ead20fc38e686a3a4267f8ae0ba1e824a0c98747
ZAMLER <jekaninho7@gmail.com>
2022-09-08 11:58:32 +0300

    ДЗ "if / else"

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,10 +1,23 @@
 ﻿
-//bool TrueFalse(int a,int b)
-//{
-//    return a % b == 0;     
-//}
-//Console.WriteLine(TrueFalse(9,2));
+bool b = int.TryParse(Console.ReadLine(),out int a);
 
-
-
-
+if (a < 15)
+{
+    Console.WriteLine("[0-14]");
+}
+else if (a < 36)
+{
+    Console.WriteLine("[15-35]");
+}
+else if (a < 51)
+{
+    Console.WriteLine("[36-50]");
+}
+else if (a < 101)
+{
+    Console.WriteLine("[50-100]");
+}
+else if (a > 100)
+{
+    Console.WriteLine("i'm not understand");
+}
