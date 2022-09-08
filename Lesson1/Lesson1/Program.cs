bc8995f2736ddd70397516ebe6cc0c29dc58a4bf
ZAMLER <jekaninho7@gmail.com>
2022-09-08 18:31:57 +0300

    масивы 2 х вымерные

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,16 +1,12 @@
-﻿int[,,] array = new int[5, 4, 2];
+﻿int[,] array = new int[5, 4];
 
-array[2, 3, 1] = 1;
+array[2, 3] = 1;
 
 for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
 {
     for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
     {
-        for (int k = 0; k < array.GetUpperBound(2) + 1; k++)
-        {
-            Console.Write(array[i, j, k]);
-        }
-        Console.WriteLine();
+        Console.Write(array[i, j]);
     }
     Console.WriteLine();
 }
