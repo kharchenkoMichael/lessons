b3c15015c8c92f8e67c879cb3b6c0fe93c3d24fb
ZAMLER <jekaninho7@gmail.com>
2022-09-08 18:57:37 +0300

    игра х / 0

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,13 +1,70 @@
-﻿int[][] array = new int[3][];
-array[0] = new int[] { 1, 2, 3 };
-array[1] = new int[] { 1, 2 };
-array[2] = new int[] { 1, 2, 3, 4 };
+﻿int[,] map = new int[3, 3];//0 - пуста клітка; 1 - хрестик; 2 - нулик;
 
-for (int i = 0; i < array.Length; i++)
+bool firstPlayer = true; // другий гравець - false
+int row = 0, column = 0;
+
+while (true)
 {
-    for (int j = 0; j < array[i].Length; j++)
+    DrawMap(map);
+    if (firstPlayer)
     {
-        Console.Write(array[i][j]);
+        Console.WriteLine("First Player turn (X)");
     }
-    Console.WriteLine();
+    else
+    {
+        Console.WriteLine("Second Player turn (0)");
+    }
+
+    Console.WriteLine("Enter row:");
+    row = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter column:");
+    column = int.Parse(Console.ReadLine());
+
+    map[row, column] = firstPlayer ? 1 : 2;
+
+    if (CheckMapState(map))
+    {
+        break;
+    }
+
+    firstPlayer = !firstPlayer;
+}
+
+void DrawMap(int[,] map)
+{
+    for (int i = 0; i < map.GetUpperBound(0) + 1; i++)
+    {
+        for (int j = 0; j < map.GetUpperBound(1) + 1; j++)
+        {
+            switch (map[i, j])
+            {
+                case 0:
+                    Console.Write(' ');
+                    break;
+                case 1:
+                    Console.Write('X');
+                    break;
+                case 2:
+                    Console.Write('O');
+                    break;
+            }
+            if (j != map.GetUpperBound(1))
+            {
+                Console.Write('|');
+            }
+        }
+
+        Console.WriteLine();
+        if (i != map.GetUpperBound(0))
+        {
+            Console.Write("-----");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool CheckMapState(int[,] map)
+{
+    //TODO: Implent method
+    return false;
 }
