07604b56822ef7d62093df1d1972180aeb2f9ecc
ZAMLER <jekaninho7@gmail.com>
2022-09-08 19:55:16 +0300

    ДЗ задача массивы,метод

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,19 +1,16 @@
 ﻿int[] arrays = { 1, 2, 3, 2, 3 };
 int ellement = 5;
-
-int[] result = new int[arrays.Length + 1];
-for (int i =0; i < arrays.Length; i++)
+int [] Method(int[] arrays, int ellement)
 {
-    result[i] = arrays[i];
+    int[] result = new int[arrays.Length + 1];
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        result[i + 1] = arrays[i];
+    }
+    result[0] = ellement;
+    return result;
 }
-
-
-
-result[result.Length - 1] = ellement;
-
-
-
-
+int[] result = Method(arrays, ellement);
 
 for (int i = 0; i < result.Length; i++)
 {
