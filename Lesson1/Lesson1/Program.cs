230f12be95ac8b6ab6ce067290498c00f54239e8
ZAMLER <jekaninho7@gmail.com>
2022-09-08 18:31:16 +0300

    масивы 2х вымерные

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,12 +1,16 @@
-﻿int.TryParse(Console.ReadLine(), out int count);
-int[] rates = new int[count];
-for (int i = 0; i < rates.Length; i++)
+﻿int[,,] array = new int[5, 4, 2];
+
+array[2, 3, 1] = 1;
+
+for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
 {
-    rates[i] = int.Parse(Console.ReadLine());
+    for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+    {
+        for (int k = 0; k < array.GetUpperBound(2) + 1; k++)
+        {
+            Console.Write(array[i, j, k]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
-int sum = 0;
-for (int i = 0; i < rates.Length; i++)
-{
-  sum = sum + rates[i];
-}
-Console.WriteLine((double) sum / rates.Length);
