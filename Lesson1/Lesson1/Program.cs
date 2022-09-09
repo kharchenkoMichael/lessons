d66b25ae54cd6e305a8e8604514a2dda0eed6004
ZAMLER <jekaninho7@gmail.com>
2022-09-09 17:23:54 +0300

    массив через метод

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,7 +1,26 @@
-﻿    int[] array = { 1, 2, 3, 4, 5 };
-    Array.Reverse(array);
-    for (int i = 0; i < array.Length; i++)
+﻿//int[] array = { 2, 5, 8, 10, 15,20,25 };
+//int index = 2;
+//int count = 3;
+//int[] SubArray(int[] array, int index, int count);
+//for (int i = 0; i > array.Length; i++)
+//{
+
+//}
+
+
+int[] array = { 1, 2, 3, 4, 5 };
+Reverse(array);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
+void Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
     {
-        Console.WriteLine(array[i]);
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
     }
-    
+}
