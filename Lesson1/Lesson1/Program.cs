5b26f84246db082e452ed370477aa25d4458252c
ZAMLER <jekaninho7@gmail.com>
2022-09-12 17:44:18 +0300

    Цыкличный Фибоначи

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -12,26 +12,24 @@
 //    return result;
 //}
 
-
-
-
-
-
-
-int Fibonachi(int n)
+int FibonachiLoop(int n)
 {
-    if (n == 0 || n == 1) return n;
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
 
-    return Fibonachi(n - 1) + Fibonachi(n - 2);
+    int result = 0;
+    int prev1 = 1, prev2 = 0;
+    for (int i = 2; i < n; i++)
+    {
+        result = prev2 + prev1;
+        prev2 = prev1;
+        prev1 = result;
+    }
+
+    return result;
 }
-
-int fib4 = Fibonachi(4);
-int fib5 = Fibonachi(5);
-int fib6 = Fibonachi(6);
-
-Console.WriteLine($"4 число Фибоначчи = {fib4}");
-Console.WriteLine($"5 число Фибоначчи = {fib5}");
-Console.WriteLine($"6 число Фибоначчи = {fib6}");
 
 
 
