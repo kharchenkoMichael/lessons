97ee1128abb9971228ce94d451bb6fd19eb903b0
ZAMLER <jekaninho7@gmail.com>
2022-09-14 18:45:46 +0300

    ДЗ 2 метод строки количесто сымволив "а"

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,54 +1,22 @@
 ﻿
-int Calculation(string str)
-{
-    int result = 0;
-    string op1 = "";
-    string op2 = "";
-    bool isOp1 = true;
-    char operation = ' ';
-    for (int i = 0; i < str.Length; i++)
-    {
-        if (str[i]== '+' || str[i]== '-' || str[i]== '*' || str[i] == '/')
-        {
-            isOp1 = false;
-            operation = str[i];
-        }
-        else 
-        {
-            if (isOp1)
-            {
-                op1 += str[i];
-            }
-            else
-            {
-                op2 += str[i];
-            }
-        }
-    }
-    int value1 = int.Parse(op1);
-    int value2 = int.Parse(op2);
-   switch (operation)
-    {
-        case '+':
-            result = value1 + value2;
-            break;
-        case '-':
-            result = value1 - value2;
-            break;
-        case '*':
-            result = value1 * value2;
-            break;
-        case '/':
-            result = value1 / value2;
-            break;
-    }
-    return result;
-}
-while (true)
-{
-    int result = Calculation(Console.ReadLine());
-    Console.WriteLine(result);
-}
+//int CountA(string str)
+//{
+//    int result = 0;
+//    for(int i = 0; i < str.Length; i++)
+//    {
+//        if (str[i]=='a')
+//        {
+//            result++;
+//        }
+//    }
+//    return result;
+
+//}
+//while (true)
+//{
+//    int result = CountA(Console.ReadLine());
+//    Console.WriteLine(result);
+//}
 
 
 
