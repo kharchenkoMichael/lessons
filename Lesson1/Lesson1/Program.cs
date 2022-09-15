680600ae13161a93bef1b365ae06fbfb26679d68
ZAMLER <jekaninho7@gmail.com>
2022-09-15 16:54:18 +0300

    case ДЗ сумма заказа

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,36 +1,33 @@
-﻿Console.WriteLine("Введите номер дня недели: 1-2-3-4-5-6-7: ");
-string day = Console.ReadLine();
-switch (day)
+﻿Console.WriteLine("размеры стаканчиков кофе: 1 = маленький, 2 = средний, 3 = большой");
+int small = 25;
+int medium = 50;
+int large = 100;
+int result = 0;
+bool exit = false;
+while (!exit)
 {
-    case "1":
-        Console.WriteLine("рабочий день");
-        break;
-    case "2":
-        Console.WriteLine("рабочий день");
-        break;
-    case "3":
-        Console.WriteLine("рабочий день");
-        break;
-    case "4":
-        Console.WriteLine("рабочий день");
-        break;
-    case "5":
-        Console.WriteLine("рабочий день");
-        break;
-    case "6":
-        Console.WriteLine("выходной");
-        break;
-    case "7":
-        Console.WriteLine("выходной");
-        break;
-    default:
-        Console.WriteLine("bye");
-        break;
-        
-
+    Console.Write("пожалуйста, сделайте свой выбор: ");
+    string coffeesize = Console.ReadLine();
+    switch (coffeesize)
+    {
+        case "1":
+            result += small;
+            break;
+        case "2":
+            result += medium;
+            break;
+        case "3":
+            result += large;
+            break;
+        case "exit":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Такого в меню нету");
+            break;
+    }
 }
-
-
+Console.WriteLine(result);
 
 
 
