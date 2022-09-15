775459a3bfb1f130cdf1f2df219d73acae73156f
ZAMLER <jekaninho7@gmail.com>
2022-09-15 17:41:16 +0300

    зашифрованные символы

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,33 +1,22 @@
-﻿int state = 0; // 0010 0000
+﻿int secretKey = 65;
 
-int TurnOn(int state, int value)
+string text = Console.ReadLine();
+
+string codeText = "";
+
+for (int i = 0; i < text.Length; i++)
 {
-    int mask = (int)Math.Pow(2, value - 1);
-    return state | mask;
+    codeText += (char)(text[i] ^ secretKey);
 }
 
-int TurnOff(int state, int value)
+Console.WriteLine(codeText);
+
+string unCodeText = "";
+for (int i = 0; i < codeText.Length; i++)
 {
-    int mask = ~(int)Math.Pow(2, value - 1);//0000 0001 -> 1111 1110
-    return state & mask;
+    unCodeText += (char)(codeText[i] ^ secretKey);
 }
 
-while (true)
-{
-    string value = Console.ReadLine();
-    if (value[0] == '+')
-    {
-        state = TurnOn(state, int.Parse(value.Substring(1).ToString()));
-    }
-    else if (value[0] == '-')
-    {
-        state = TurnOff(state, int.Parse(value.Substring(1).ToString()));
-    }
-    else
-    {
-        Console.WriteLine("I don't understand");
-    }
-    Console.WriteLine($"{Convert.ToString(state, 2)}");
-}
+Console.WriteLine(unCodeText);
 
 
