4191e5d54205458e3e994ce20f6170b87493ef79
ZAMLER <jekaninho7@gmail.com>
2022-09-15 17:43:59 +0300

    показывает случайное число

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,22 +1,4 @@
-﻿int secretKey = 65;
-
-string text = Console.ReadLine();
-
-string codeText = "";
-
-for (int i = 0; i < text.Length; i++)
-{
-    codeText += (char)(text[i] ^ secretKey);
-}
-
-Console.WriteLine(codeText);
-
-string unCodeText = "";
-for (int i = 0; i < codeText.Length; i++)
-{
-    unCodeText += (char)(codeText[i] ^ secretKey);
-}
-
-Console.WriteLine(unCodeText);
+﻿Random random = new Random();
+Console.WriteLine(random.Next(-50, 50));
 
 
