96f2ac9e786334a05e17023409c4dd930bd37d84
ZAMLER <jekaninho7@gmail.com>
2022-09-15 18:09:37 +0300

    Игра угадай число

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,4 +1,25 @@
-﻿Random random = new Random();
-Console.WriteLine(random.Next(-50, 50));
+﻿using System.Data;
+
+Random random = new Random();
+int value = random.Next(100);
+Console.WriteLine("Угадай число от 0-100");
+while (true)
+{
+    int a = int.Parse(Console.ReadLine());
+    if(a < value)
+    {
+        Console.WriteLine($"Загаданное число больше {a}");
+    }
+    else if (a > value)
+    {
+        Console.WriteLine($"Загадонное чило меньше {a}");
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine("Ты угадал");
+
 
 
