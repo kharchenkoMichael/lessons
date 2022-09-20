e3899f48cfaf8f70ddff5c31bf9e5fa312c73749
ZAMLER <jekaninho7@gmail.com>
2022-09-20 18:53:54 +0300

    КЛАССНАЯ РАБОТА высчитывали зарплату и налог в зависимости от опыта работы

---
+++ Lesson1/Lesson1/Employee.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Lesson1
+{
+   public class Employee
+    {
+        private readonly string name;
+        private readonly string lastName;
+        public string JobTitle { get; set; }
+        public int YearsInCompany { get; set; }
+
+        public Employee(string name, string lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+        public int CalculateSallary()
+        {
+            int sallary = 0;
+            switch (JobTitle)
+            {
+                case "junior":
+                    sallary = 200 + YearsInCompany * 50;
+                    break;
+                case "middle":
+                    sallary = 1500 + YearsInCompany * 100;
+                    break;
+                case "senior":
+                    sallary = 4000 + YearsInCompany * 200;
+                    break;
+              
+            }
+            return sallary;
+        }
+        public void ShowInfo()
+        {
+            var sallary = CalculateSallary();
+            Console.WriteLine($"ім'я  {name}, прізвище {lastName}, посада {JobTitle}, зарплатня {sallary}, податок {sallary / 100 * 5}");
+        }
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,18 +1,6 @@
 ﻿using Lesson1;
-using System;
 
-var apple = new Apple();
-var banana = new Banana();
-
-apple.Eat();
-banana.Eat();
-
-Console.WriteLine("3 days after");
-
-apple.Spoil();
-banana.Spoil();
-
-apple.Eat();
-banana.Eat();
-
-var appleSpoiled = new Apple(true);
+var employee = new Employee("Serhii","Bychkov");
+employee.JobTitle = "middle";
+employee.YearsInCompany = 5;
+employee.ShowInfo();
