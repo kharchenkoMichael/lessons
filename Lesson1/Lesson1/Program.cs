3d98a762f6701b87091416edddf9936b4f148390
ZAMLER <jekaninho7@gmail.com>
2022-09-20 13:34:34 +0300

    Дз - User инфо

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Lesson1;
-var figure = new Figure(new Point(3, 2, ".a"),new Point(4,5,".b"),new Point(6,8,".c"),new Point(-2,-5,"d"));
-figure.PerimeterCalculator();
-var result = figure.LengthSide(new Point(1,2,"a"),new Point(-1,-3,"b"));
-Console.WriteLine(result);
+User user = new User();
+user.Login = "jakson";
+user.Name = "Jeka";
+user.surname = "Murashov";
+user.Age = 27;
+user.Date = DateTime.Now;
+Console.WriteLine($" логин - {user.Login}, имя - {user.name}, фамилия - {user.surname}, возвраст - {user.age},дата - {user.Date}");
+
