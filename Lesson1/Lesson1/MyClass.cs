726af86238e87f73bbbedad60d41ae90edcaa0b8
ZAMLER <jekaninho7@gmail.com>
2022-09-22 17:32:47 +0300

    Дз2  выводы на Console

---
+++ Lesson1/Lesson1/MyClass.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+namespace Lesson1
+{
+    public class MyClass
+    {
+        private string field;
+        
+
+
+        public string Fieald
+        {
+            get
+            {
+                if (field != null)
+                {
+                    if(field == "hello world")
+                    {
+                        return "HELLO WORLD!";
+                    }
+                    return field;
+                   
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+            set
+            {
+                if(value == "goodbye")
+                {
+                    Console.WriteLine("Вы ввели недопустимое значение");
+                }
+                else
+                {
+                    field = value;
+                }
+            }
+
+        }
+
+
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,20 +1,10 @@
 ﻿using Lesson1;
-
-var chair = new Chair();
-var table = new Table();
-var sofa = new Sofa();
-
-chair.FurnitureCrashed();
-table.FurnitureCrashed();
-sofa.FurnitureCrashed();
-
-chair.Broke();
-table.Broke();
-sofa.Broke();
-
-Console.WriteLine("after the fall");
-
-chair.FurnitureCrashed();
-table.FurnitureCrashed();
-sofa.FurnitureCrashed();
-
+var myclass = new MyClass();
+myclass.Fieald = "goodbye";
+Console.WriteLine(myclass.Fieald);
+myclass.Fieald = null;
+Console.WriteLine(myclass.Fieald);
+myclass.Fieald = "hello world";
+Console.WriteLine(myclass.Fieald);
+myclass.Fieald = "HAPPY NEW YEAR";
+Console.WriteLine(myclass.Fieald);
