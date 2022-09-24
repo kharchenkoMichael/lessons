4608670e6b7e4e78da84b6380fcb60e1248f94d2
ZAMLER <jekaninho7@gmail.com>
2022-09-24 17:22:00 +0300

    Дз DocumenWorker

---
+++ Lesson1/Lesson1/ExpertDocumentWorker.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Lesson1
+{
+    public class ExpertDocumentWorker :ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,11 +1,18 @@
 ﻿using Lesson1;
-
-var car = new Car(10,15,1000,100,2015);
-var plane = new Plane(150.5, 300, 50, 76, 50000, 800, 2020);
-var ship = new Ship("Venezuela", 5000, 500, 1000, 1000000, 123.7, 2022);
-car.ShowInfo();
-Console.WriteLine("************");
-plane.ShowInfo();
-Console.WriteLine("************");
-ship.ShowInfo();
-        
+string str = Console.ReadLine();
+DocumentWorker worker;
+switch (str)
+{
+    case "pro":
+        worker = new ProDocumentWorker();
+        break;
+    case "exp":
+        worker = new ExpertDocumentWorker();
+        break ;
+    default:
+        worker = new DocumentWorker();
+        break;
+}
+worker.OpenDocument();
+worker.SaveDocument();
+worker.EditDocument();
