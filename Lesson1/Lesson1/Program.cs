cb58f0f140cc3239dba70b4f40c1d71889987f1b
ZAMLER <jekaninho7@gmail.com>
2022-09-26 18:47:00 +0300

    Классная Работа сделал index прошелся по 3 миссивам нашел мин числа макс число и сумму всех числе массива

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,17 +1,5 @@
 ﻿using Lesson1;
-
-var customArray = new CustomArray(new int[] { 1, 5, 23, 7, 5 }, new int[] { 2, 234, 12, -1, 43, 9 });
-for (int i = 0; i < customArray.Lenght; i++)
-{
-    Console.Write($"{customArray[i]}, ");
-    if (i % 2 == 0)
-    {
-        customArray[i] = -customArray[i];
-    }
-}
-Console.WriteLine();
-Console.WriteLine("----------------------");
-for (int i = 0; i < customArray.Lenght; i++)
-{
-    Console.Write($"{customArray[i]}, ");
-}
+var wrapperArray = new WrapperArray(new int[] { 1, 34, 55, 12, 15 }, new int[] { 0, 77, 88, 31, 23, 44, 55 }, array3: new int[] { 66, 11, -1, 34, 62, 110 });
+Console.WriteLine(wrapperArray.Min());
+Console.WriteLine(wrapperArray.Max());
+Console.WriteLine(wrapperArray.Sum());
