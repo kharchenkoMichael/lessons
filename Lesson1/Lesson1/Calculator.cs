6c82d83b8e9938165d1429de60ba6fbec0b979cc
ZAMLER <jekaninho7@gmail.com>
2022-09-27 18:54:33 +0300

    Метод разширения строки который возвращает обратную строку

---
+++ Lesson1/Lesson1/Calculator.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Lesson1
+{
+    public static class Calculator
+    {
+        public static string Reverse(this string str)
+        {
+            var result = "";
+            for(int i = str.Length -1; i >= 0; i--)
+            {
+              result += str[i];
+            }
+          return result;
+        }
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,6 +1,3 @@
 ﻿using Lesson1;
-var store = new Store(new Article[] 
-{ new Article("ковбаса", "АТБ", 25.70), 
-    new Article("морозиво", "МакДональдс", 30.50), 
-    new Article("хліб", "МаркетОпт", 14.25) });
-Console.WriteLine(store["шоколад"]);
+var str = "asdadwqeqwe";
+Console.WriteLine(str.Reverse());
