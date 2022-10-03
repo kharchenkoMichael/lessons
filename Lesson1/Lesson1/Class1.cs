d9e9db353dff1f62b56cd52c33c0ddce0392d3c1
ZAMLER <jekaninho7@gmail.com>
2022-10-03 18:31:45 +0300

    Generic Array - обобщение массива

---
+++ Lesson1/Lesson1/Class1.cs
@@ -0,0 +1,88 @@
+﻿namespace Lesson1
+{
+    public class Class1<T> : Object
+    {
+        private T[] array;
+        private int lenght;
+
+        public int Length
+        {
+            get
+            {
+                return lenght;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public Class1()
+        {
+            lenght = 0;
+            array = new T[1];
+        }
+
+        public void Add(T newValue)
+        {
+            if (lenght < array.Length)
+            {
+                array[lenght++] = newValue;
+                return;
+            }
+
+            var newArray = new T[array.Length * 2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[lenght++] = newValue;
+            array = newArray;
+        }
+
+        public void Remove(T value)
+        {
+            var index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+            {
+                return;
+            }
+            RemoveAtIndex(index);
+        }
+
+        public void RemoveAtIndex(int index)
+        {
+            for (int i = index + 1; i < lenght; i++)
+            {
+                array[i - 1] = array[i];
+            }
+            lenght--;
+        }
+
+        public void ShowElements()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,17 +1,32 @@
-﻿var method = (Func<int>[] arrayFunc) =>
-{
-    var sum = 0;
-    for (int i = 0; i < arrayFunc.Length; i++)
-    {
-        sum += arrayFunc[i]();
-    }
-    return sum / arrayFunc.Length;
+﻿using Lesson1;
 
+var ints = new Class1<int>();
+ints.Add(2);
+ints.Add(89);
+ints.Add(3);
+ints.Add(2);
+ints.Add(3);
+ints.Add(5);
+ints.ShowElements();
+ints.Remove(3);
+ints.ShowElements();
+ints.RemoveAtIndex(1);
+ints.ShowElements();
 
+ints.Remove(0);
+ints.ShowElements();
 
-};
-Console.WriteLine(method(new Func<int>[] { () => 5, () => 7, () => { var random = new Random();
-    var n = random.Next(0,100);
-Console.WriteLine(n);
-return n; } }));
+var strings = new Class1<string>();
+strings.Add("234adfa1");
+strings.Add("aqdf243");
+strings.Add("rftgyhdrt");
+strings.Add("4567");
+strings.Add("qwe");
+strings.Add("678");
+strings.ShowElements();
+
+strings.Remove("qwe");
+strings.ShowElements();
+strings.RemoveAtIndex(1);
+strings.ShowElements();
 
