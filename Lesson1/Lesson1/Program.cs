4613aba9010f2e0d448d23944b211276f66d32d9
ZAMLER <jekaninho7@gmail.com>
2022-10-03 17:36:01 +0300

    Создание лямбда оператора для выполнения арифметических действий:

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,2 +1,9 @@
 ﻿
-var method = (int x, int y, int z) => (x + y + z) / 3;
+
+
+var Add = (int x, int y) => x + y;
+var Sub = (int x, int y) => x - y;
+var Mul = (int x, int y) => x * y;
+var Div = (int x, int y) => x / y;
+var Method = (Func<int, int, int> Function, int x, int y) => Function(x, y);
+Console.WriteLine(Method(Div,5,8));
