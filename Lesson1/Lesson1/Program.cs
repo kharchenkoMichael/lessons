d9fb4bffdf07eb2743765097c8814f5c6d36ed6c
ZAMLER <jekaninho7@gmail.com>
2022-10-05 10:45:39 +0300

    Пример использования arrayList

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,14 +1,10 @@
-﻿using Lesson1;
-MyList<int> myList = new MyList<int>();
-myList.Add(5);
-myList.Add(8);
-myList.Add(11);
-myList.Add(72);
-myList.Add(99);
-var array = myList.GetArray();
-for(int i = 0; i < array.Length; i++)
+﻿using System.Collections;
+
+var arrayList = new ArrayList();
+arrayList.Add(5);
+arrayList.Add("String");
+for(int i = 0; i < arrayList.Count; i++)
 {
-    Console.WriteLine(array[i]);
+    var item = arrayList[i];
+    Console.WriteLine(item);
 }
-
-
