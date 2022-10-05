c432492c0418eeaa88c5338ad01942196a7a1715
ZAMLER <jekaninho7@gmail.com>
2022-10-05 09:14:26 +0300

    ДЗ 1 MyList<T>

---
+++ Lesson1/Lesson1/MyClass.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Lesson1
+{
+    public class MyList <T>
+    {
+        private T[] array;
+
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public int Lenght
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public void Add(T value)
+        {
+            var newArray = new T[array.Length + 1];
+            for(int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[newArray.Length - 1] = value;
+            array = newArray;
+        } 
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,32 +1,4 @@
 ﻿using Lesson1;
 
-var ints = new Class1<int>();
-ints.Add(2);
-ints.Add(89);
-ints.Add(3);
-ints.Add(2);
-ints.Add(3);
-ints.Add(5);
-ints.ShowElements();
-ints.Remove(3);
-ints.ShowElements();
-ints.RemoveAtIndex(1);
-ints.ShowElements();
 
-ints.Remove(0);
-ints.ShowElements();
-
-var strings = new Class1<string>();
-strings.Add("234adfa1");
-strings.Add("aqdf243");
-strings.Add("rftgyhdrt");
-strings.Add("4567");
-strings.Add("qwe");
-strings.Add("678");
-strings.ShowElements();
-
-strings.Remove("qwe");
-strings.ShowElements();
-strings.RemoveAtIndex(1);
-strings.ShowElements();
 
