da956ff1eca7df9b33c1c9a8a5561894d9b21984
ZAMLER <jekaninho7@gmail.com>
2022-10-06 09:21:06 +0300

    Дз ArrayList

---
+++ Lesson1/Lesson1/ArrayList.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Lesson1
+{
+    public class ArrayList
+    {
+        private object[] array = new object[0];
+
+
+
+
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+        public void Add(object value)
+        {
+            var newArray = new object[array.Length + 1];
+            for(int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[newArray.Length-1] = value;
+            array = newArray;
+        }
+        public void Show()
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-
+﻿using Lesson1;
 var arrayList = new ArrayList();
 arrayList.Add(5);
-arrayList.Add("String");
-for(int i = 0; i < arrayList.Count; i++)
-{
-    var item = arrayList[i];
-    Console.WriteLine(item);
-}
+arrayList.Add("hello");
+arrayList.Add('c');
+arrayList.Add(true);
+arrayList.Add(5.67);
+arrayList.Show();
+Console.WriteLine("-----------");
+arrayList[1] = false;
+arrayList.Show();
+
+
