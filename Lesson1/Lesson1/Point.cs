599776d70eb37328351aa194c8b5112c9f85a920
ZAMLER <jekaninho7@gmail.com>
2022-10-13 18:13:52 +0300

    Классная работа оператор задача 2

---
+++ Lesson1/Lesson1/Point.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lesson1
+{
+    public struct Point
+    {
+        private int x;
+        private int y;
+        private int z;
+        public Point(int x,int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            
+        }
+        public static Point operator+(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{x}, {y}, {z}");
+        }
+
+    }
+}
--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,22 +1,10 @@
 ﻿
 using Lesson1;
-
-var a = new Vektor() { X = 2, Y = 3 };
-var b = new Vektor() { X = 5, Y = 1 };
+var a = new Point(3,1,5);
+var b = new Point(2,-1,7);
 var c = a + b;
-a.Show("A");
-b.Show("B");
-c.Show("C");
-var d = c - new Vektor(2, 2);
-d.Show("D");
-
-var e = a * 5;
-e.Show("E");
-var f = new Vektor(2, 3);
-Console.WriteLine(a == f);
-Console.WriteLine(b == f);
-double lenght = a;
-Console.WriteLine(lenght);
-
+a.ShowInfo();
+b.ShowInfo();
+c.ShowInfo();
 
 
