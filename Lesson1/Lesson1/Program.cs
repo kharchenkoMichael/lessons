9c84ec776db3a0b62a73f7a20cb0c7bc451174dc
ZAMLER <jekaninho7@gmail.com>
2022-10-13 18:44:56 +0300

    Классная работа задача 3 operator +,-

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,11 +1,6 @@
 ﻿
 using Lesson1;
-var block = new Block(2,5,3,6);
-var block1 = new Block(2, 5, 3, 6);
-var block2 = new Block(-1,7,12,14);
-Console.WriteLine(block);
-Console.WriteLine(block1);
-Console.WriteLine(block2);
-Console.WriteLine(block.Equals(block1));
-Console.WriteLine(block.Equals(block2));
-
+var date = new Date(13, 10, 2022);
+var date1 = new Date(9, 5, 1995);
+Console.WriteLine(date - date1);
+Console.WriteLine(date + 90);
