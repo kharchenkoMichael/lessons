ef89675af72e5fc16edee5c059861dacf0086b3b
ZAMLER <jekaninho7@gmail.com>
2022-10-19 17:52:05 +0300

    Классная работа информация о DriveInfo

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,15 +1,14 @@
-﻿using Lesson1;
-using System.Runtime.CompilerServices;
+﻿using System.IO;
 
-List<Client> clients = new List<Client>
+var drives = DriveInfo.GetDrives();
+foreach (var item in drives)
 {
-    new Client() {Name = "Katya", Phone = "black", Date = new DateTime(2022,7,15,17,0,0), Price = 350},
-    new Client() {Name = "Juli", Phone = "white", Date = new DateTime(2022,5,17,13,0,0), Price = 400},
-    new Client() {Name = "Anna", Phone = "black", Date = new DateTime(2022,11,22,14,0,0), Price = 200},
-    new Client() {Name = "Nastya", Phone = "grey", Date = new DateTime(2022,9,1,11,0,0), Price = 150},
-    new Client() {Name = "Mia", Phone = "yellow", Date = new DateTime(2022,8,2,16,30,0), Price = 800},
-    new Client() {Name = "Marina", Phone = "white", Date = new DateTime(2022,3,29,12,45,0), Price = 900},
-    new Client() {Name = "Ksusha", Phone = "pink", Date = new DateTime(2022,12,16,19,15,0), Price = 500} 
-};
-var result = clients.Average(x => x.Price);
-Console.WriteLine(result);
+    Console.WriteLine(item.Name);
+    Console.WriteLine(item.DriveType);
+    Console.WriteLine(item.VolumeLabel);
+    Console.WriteLine(item.TotalFreeSpace);
+    Console.WriteLine(item.TotalSize);
+    Console.WriteLine(item.DriveFormat);
+    Console.WriteLine(item.IsReady);
+    Console.WriteLine(item.RootDirectory);
+}
