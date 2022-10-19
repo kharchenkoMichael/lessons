33fd6513e2eb91de611c614e6a8b45f5795e4fb4
ZAMLER <jekaninho7@gmail.com>
2022-10-19 18:51:07 +0300

    классная работа StreamWriter , StreamReader

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -3,15 +3,26 @@
 var directory = Directory.GetCurrentDirectory();
 for (int i = 0; i < 10; i++)
 {
-    using (var file = File.Create(Path.Combine(directory, $"File_{i + 1}.txt")))
+    var content = "";
+    using (var reader = new StreamReader(Path.Combine(directory, $"File_{i + 1}.txt")))
     {
-        string text = "some text" + i;
-        var buffer = text.ToCharArray().Select(item => (byte)item).ToArray();
-        file.Write(buffer, 0, buffer.Length);
+        content = reader.ReadToEnd();
     }
+
+    using (var writer = new StreamWriter(Path.Combine(directory, $"File_{i + 1}.txt")))
+    {
+        writer.WriteLine(content);
+        writer.Write("New Line");
+    }
+    //using (var file = File.Create(Path.Combine(directory, $"File_{i + 1}.txt")))
+    //{
+    //    string text = "some text" + i;
+    //    var buffer = text.ToCharArray().Select(item => (byte)item).ToArray();
+    //    file.Write(buffer, 0, buffer.Length);
+    //}
 }
 
-for (int i = 0; i < 10; i++)
-{
-    File.Delete(Path.Combine(directory, $"File_{i + 1}.txt"));
-}
+//for (int i = 0; i < 10; i++)
+//{
+//    File.Delete(Path.Combine(directory, $"File_{i + 1}.txt"));
+//}
