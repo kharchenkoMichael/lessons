eb3f5d08e1e03561b733b36f771180ca4ef70eae
ZAMLER <jekaninho7@gmail.com>
2022-10-19 16:47:35 +0300

    Дз средняя цена работ

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -11,8 +11,5 @@ List<Client> clients = new List<Client>
     new Client() {Name = "Marina", Phone = "white", Date = new DateTime(2022,3,29,12,45,0), Price = 900},
     new Client() {Name = "Ksusha", Phone = "pink", Date = new DateTime(2022,12,16,19,15,0), Price = 500} 
 };
-var result = clients.OrderBy(x => x.Date).Skip(3) ;
-foreach (var item in result)
-{
-    Console.WriteLine($"{item.Name}, {item.Phone}, {item.Date}, {item.Price}");
-}
+var result = clients.Average(x => x.Price);
+Console.WriteLine(result);
