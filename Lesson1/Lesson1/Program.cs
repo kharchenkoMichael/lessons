638784b423dc20123eeb204a826f1c18b7e04fc6
ZAMLER <jekaninho7@gmail.com>
2022-10-19 19:12:20 +0300

    Классная работа создание и описание в середине ZIP файла

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,28 +1,12 @@
 ﻿using System.IO;
 
-var directory = Directory.GetCurrentDirectory();
-for (int i = 0; i < 10; i++)
-{
-    var content = "";
-    using (var reader = new StreamReader(Path.Combine(directory, $"File_{i + 1}.txt")))
-    {
-        content = reader.ReadToEnd();
-    }
+using System.IO;
+using System.IO.Compression;
 
-    using (var writer = new StreamWriter(Path.Combine(directory, $"File_{i + 1}.txt")))
-    {
-        writer.WriteLine(content);
-        writer.Write("New Line");
-    }
-    //using (var file = File.Create(Path.Combine(directory, $"File_{i + 1}.txt")))
-    //{
-    //    string text = "some text" + i;
-    //    var buffer = text.ToCharArray().Select(item => (byte)item).ToArray();
-    //    file.Write(buffer, 0, buffer.Length);
-    //}
-}
-
-//for (int i = 0; i < 10; i++)
-//{
-//    File.Delete(Path.Combine(directory, $"File_{i + 1}.txt"));
-//}
+using (var writer = new StreamWriter(new FileStream("new-file.txt", FileMode.OpenOrCreate)))
+{ writer.Write("Some text"); }
+using var zip = new GZipStream(new FileStream("new-file.zip", FileMode.OpenOrCreate), CompressionLevel.SmallestSize);
+using var reader = (new FileStream("new-file.txt", FileMode.OpenOrCreate));
+var buffer = new byte[reader.Length];
+reader.Read(buffer);
+zip.Write(buffer);
