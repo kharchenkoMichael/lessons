71f2de47067eed6e046cb070c5cc67b5507498ab
ZAMLER <jekaninho7@gmail.com>
2022-10-19 18:21:21 +0300

    Классная работа создание папок

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,23 +1,11 @@
 ﻿using System.IO;
 
-var drives = DriveInfo.GetDrives();
-foreach (var item in drives)
+var directory = Directory.GetCurrentDirectory();
+for (int i = 0; i < 10; i++)
 {
-    var directory = new DirectoryInfo(item.Name);
-    var files = directory.GetFiles();
-    foreach (var file in files)
-    {
-        Console.WriteLine(file.FullName);
-        Console.WriteLine(file.Name);
-        Console.WriteLine(file.Length);
-        Console.WriteLine(file.LinkTarget);
-        Console.WriteLine(file.CreationTime);
-        Console.WriteLine(file.DirectoryName);
-        Console.WriteLine(file.Exists);
-        Console.WriteLine(file.Extension);
-        Console.WriteLine(file.IsReadOnly);
-        Console.WriteLine(file.LastAccessTime);
-        Console.WriteLine(file.LastWriteTime);
-        Console.WriteLine("--------------");
-    }
+    Directory.CreateDirectory(Path.Combine(directory, $"New_Directory_{i + 1}"));
+}
+for (int i = 0; i < 10; i++)
+{
+    Directory.Delete(Path.Combine(directory, $"New_Directory_{i + 1}"));
 }
