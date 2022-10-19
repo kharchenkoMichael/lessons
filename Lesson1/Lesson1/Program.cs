6daf602986c86a61cdeb40e2b391fe2c398a6a1d
ZAMLER <jekaninho7@gmail.com>
2022-10-19 18:01:19 +0300

    Классная работа информация про ПАПКИ

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -3,12 +3,20 @@
 var drives = DriveInfo.GetDrives();
 foreach (var item in drives)
 {
-    Console.WriteLine(item.Name);
-    Console.WriteLine(item.DriveType);
-    Console.WriteLine(item.VolumeLabel);
-    Console.WriteLine(item.TotalFreeSpace);
-    Console.WriteLine(item.TotalSize);
-    Console.WriteLine(item.DriveFormat);
-    Console.WriteLine(item.IsReady);
-    Console.WriteLine(item.RootDirectory);
+    var directory = new DirectoryInfo(item.Name);
+    var directories = directory.GetDirectories();
+    foreach (var d in directories)
+    {
+        Console.WriteLine(d.FullName);
+        Console.WriteLine(d.Name);
+        Console.WriteLine(d.LinkTarget);
+        Console.WriteLine(d.Parent?.Name);
+        Console.WriteLine(d.CreationTime);
+        Console.WriteLine(d.Exists);
+        Console.WriteLine(d.Extension);
+        Console.WriteLine(d.LastAccessTime);
+        Console.WriteLine(d.LastWriteTime);
+        Console.WriteLine(d.Root);
+        Console.WriteLine("-------------------------");
+    }
 }
