2805793471772b50f5cfec18be3572fecbd24cd4
ZAMLER <jekaninho7@gmail.com>
2022-10-20 18:58:42 +0300

    Классная Работа Reflexia прошолся по всем проперти использывал Aggregate

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,9 +1,8 @@
-﻿using System.Reflection;
+﻿using Lesson1;
 
-var assembly = Assembly.GetExecutingAssembly();
-var types = assembly.GetTypes();
-foreach (var type in types)
-{
-    Console.WriteLine(type.Name);
-}
+var user1 = new User() { FirstName = "Jenya", LastName = "Murashov", Age = 27, City = "Kremenchuk", Mail = "sadas@qwe.com"};
+var user2 = new User() { FirstName = "Misha", LastName = "Kharchenko", Age = 24, City = "Kremenchuk", Mail = "12312@lke.net" };
+Console.WriteLine(user1);
+Console.WriteLine(user2);
+
 
---
+++ Lesson1/Lesson1/User.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Lesson1
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Mail { get; set; }
+
+        public override string ToString()
+        { 
+            var type = GetType();
+            return type.GetProperties().Select(property => property.GetValue(this)?.ToString()).Aggregate((prop1, prop2) => $"{prop1}, {prop2}"); 
+        }
+    }
+}
