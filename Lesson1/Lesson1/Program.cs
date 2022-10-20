c9bc788430a40f34e74c9657b006f3064dbeb9a0
ZAMLER <jekaninho7@gmail.com>
2022-10-20 18:20:39 +0300

    Классная работа что такое Reflexia

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,24 +1,54 @@
-﻿using System.IO;
-using System.IO.Compression;
-using System.IO.IsolatedStorage;
+﻿using System.Reflection;
+using Lesson1;
 
-var folderPath = Console.ReadLine();
-var directory = new DirectoryInfo(folderPath);
-var fileName = Console.ReadLine();
-IEnumerable<FileInfo> ConteinFileInFolder (DirectoryInfo directory, string fileName)
+var myClass = new Class1();
+Type type = typeof(Class1);
+PropertyInfo[] properties = type.GetProperties();
+foreach (var property in properties)
 {
-    var result = new List<FileInfo>();
-    result.AddRange(directory.GetFiles().Where(f => f.Name.Contains(fileName)));
-    foreach (var item in directory.GetDirectories())
+    Console.WriteLine(property.Name);
+    Console.WriteLine(property.PropertyType);
+    if (property.GetIndexParameters().Length > 0)
     {
-        result.AddRange(ConteinFileInFolder(item, fileName));
+        Console.WriteLine(property.GetValue(myClass, new object[] { 4 }));
     }
-    return result;
+    else
+    {
+        Console.WriteLine(property.GetValue(myClass));
+    }
 }
-var result = ConteinFileInFolder(directory, fileName);
-foreach(var item in result)
+var fields = type.GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance | BindingFlags.Public);
+foreach (var field in fields)
 {
-    Console.WriteLine(item.FullName);
+    Console.WriteLine(field.Name);
+    Console.WriteLine(field.GetValue(myClass));
+    Console.WriteLine(field.FieldType);
+    Console.WriteLine(field.MemberType);
+}
+myClass.ShowInfo();
+var methods = type.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+foreach (var method in methods)
+{
+    Console.WriteLine(method.Name);
+    Console.WriteLine(method.ReturnType);
+    Console.WriteLine(method.GetParameters().Length);
+}
+EventInfo[] events = type.GetEvents();
+foreach (var e in events)
+{
+    Console.WriteLine(e.Name);
+    Console.WriteLine(e.EventHandlerType);
+}
+var constructors = type.GetConstructors();
+foreach (var constructor in constructors)
+{
+    Console.WriteLine(constructor.Name);
+}
+var members = type.GetMembers(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+foreach (var member in members)
+{
+    Console.WriteLine($"{member.Name} - {member.MemberType}");
 }
 
 
