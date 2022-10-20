f1b3bacadc14624db4a9bfdc668d3696225b36e0
ZAMLER <jekaninho7@gmail.com>
2022-10-20 18:36:50 +0300

    Классная работа рефлектор получение информации вхожящих в ее состав

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -1,54 +1,9 @@
 ﻿using System.Reflection;
-using Lesson1;
 
-var myClass = new Class1();
-Type type = typeof(Class1);
-PropertyInfo[] properties = type.GetProperties();
-foreach (var property in properties)
+var assembly = Assembly.GetExecutingAssembly();
+var types = assembly.GetTypes();
+foreach (var type in types)
 {
-    Console.WriteLine(property.Name);
-    Console.WriteLine(property.PropertyType);
-    if (property.GetIndexParameters().Length > 0)
-    {
-        Console.WriteLine(property.GetValue(myClass, new object[] { 4 }));
-    }
-    else
-    {
-        Console.WriteLine(property.GetValue(myClass));
-    }
+    Console.WriteLine(type.Name);
 }
-var fields = type.GetFields(BindingFlags.NonPublic |
-                         BindingFlags.Instance | BindingFlags.Public);
-foreach (var field in fields)
-{
-    Console.WriteLine(field.Name);
-    Console.WriteLine(field.GetValue(myClass));
-    Console.WriteLine(field.FieldType);
-    Console.WriteLine(field.MemberType);
-}
-myClass.ShowInfo();
-var methods = type.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-foreach (var method in methods)
-{
-    Console.WriteLine(method.Name);
-    Console.WriteLine(method.ReturnType);
-    Console.WriteLine(method.GetParameters().Length);
-}
-EventInfo[] events = type.GetEvents();
-foreach (var e in events)
-{
-    Console.WriteLine(e.Name);
-    Console.WriteLine(e.EventHandlerType);
-}
-var constructors = type.GetConstructors();
-foreach (var constructor in constructors)
-{
-    Console.WriteLine(constructor.Name);
-}
-var members = type.GetMembers(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-foreach (var member in members)
-{
-    Console.WriteLine($"{member.Name} - {member.MemberType}");
-}
-
 
